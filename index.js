a5d28e39bc5e18c23c26d699df8c67607c5c2cc1
mumtaza <mumtazmohamed453@gmail.com>
2025-06-22 13:24:39 +0300

    function continued done

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function saturdayFun(activity = "roller-skate") {
-    return This Saturday, I want to ${activity}!;
-  }
+  return `This Saturday, I want to ${activity}!`;
+}
 
-   function mondayWork(activity = "go to the office") {
-    return This Monday, I will ${activity}.;
-  }
-function wrapAdjective (flair="*"){
-  return function(adjective = "special"){
-    return You are ${flair}${adjective}${flair}!;
-  }
+  function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
 
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
 }
